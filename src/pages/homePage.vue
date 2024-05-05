23f0fcf2ea137427191a98d6e98d33f310d02fcd
aliengigant <hakankanak@hotmail.de>
2024-05-05 19:09:13 +0200

    Homepage angepasst

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,40 +1,61 @@
 <template>
   <div class="bg-darkblue">
     <div class="row justify-content-center">
-      <div class="bg-primary">
+      <div class="bg-primary text-center">
         <div class="red" @click="easterEggCounter++">
-          <h1 class="display-1" style="color: aliceblue">Trafo Tool</h1>
+          <h1 class="display-1" style="color: aliceblue">AutomataLab</h1>
           <p class="lead" style="color: aliceblue">Ein Tool für dein Fantasy</p>
         </div>
       </div>
-      <div class="row justify-content-center gy-5">
-        <div class="col-md-auto text-center">
-          <a href="/automatOverview">
-            <div class="card" style="width: 15rem">
-              <img src="@/assets/auto.jpg" class="card-img-top" alt="..." />
-              <div class="class-body m-3">
-                <h5 class="card-title">Automat</h5>
-                <p class="card-text">Erstelle deinen Automaten</p>
-              </div>
-            </div></a
-          >
-        </div>
-        <div class="col-md-auto text-center">
-          <a href="/grammatikOverview">
-            <div class="card" style="width: 15rem">
-              <img
-                src="@/assets/grammatik.jpg"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="class-body m-3">
-                <h5 class="card-title">Grammatik</h5>
-                <p class="card-text">Erstelle deinen Grammatik</p>
-              </div>
-            </div></a
-          >
-        </div>
-        <!-- <div class="col-md-auto">
+    </div>
+    <div class="row justify-content-center">
+      <hr />
+      <figure class="text-center">
+        <h5 style="color: aliceblue">
+          <p class="lead">Über AutomataLab</p>
+
+          Willkommen beim Online-Tool für "Theoretische Informatik und Logik"
+          von Professor Dr. Faßbender an der FH Aachen!
+          <br />
+          <br />
+          Diese Webseite ist deine virtuelle Schatzkarte für die faszinierende
+          Welt der Automatenlehre und Grammatik.
+          <br />
+          <br />
+          Ob du gerade erst in die Materie eintauchst oder bereits ein
+          erfahrener Entdecker bist, hier findest du alles, was du brauchst, um
+          die komplexen Konzepte zu verstehen und zu beherrschen.
+          <br />
+          <br />
+          Unser interaktives Tool bietet dir:
+        </h5>
+      </figure>
+      <hr />
+    </div>
+    <div class="row justify-content-center gy-1">
+      <div class="col-md-auto text-center">
+        <a href="/automatOverview">
+          <div class="card" style="width: 15rem">
+            <img src="@/assets/auto.jpg" class="card-img-top" alt="..." />
+            <div class="class-body m-3">
+              <h5 class="card-title">Automat</h5>
+              <p class="card-text">Erstelle deinen Automaten</p>
+            </div>
+          </div></a
+        >
+      </div>
+      <div class="col-md-auto text-center">
+        <a href="/grammatikOverview">
+          <div class="card" style="width: 15rem">
+            <img src="@/assets/grammatik.jpg" class="card-img-top" alt="..." />
+            <div class="class-body m-3">
+              <h5 class="card-title">Grammatik</h5>
+              <p class="card-text">Erstelle deinen Grammatik</p>
+            </div>
+          </div></a
+        >
+      </div>
+      <!-- <div class="col-md-auto">
             <div class="card" style="width: 15rem">
               <div class="class-body">
                 <img src="@/assets/logo.png" class="card-img-top" alt="..." />
@@ -44,40 +65,33 @@
               </div>
             </div>
           </div> -->
-      </div>
     </div>
-    <div v-if="easterEggCounter >= 7">
-      <p>lol du hast mich gefunden</p>
-      <p>Gib nun den einzige wahren Cheat Code ein</p>
-      <!-- Div-Container für die Felder -->
-      {{ codeString }}
-      <div id="fieldsContainer" v-if="code.length < 10">
-        <!-- 8 Felder mit IDs field1 bis field8 -->
-        <button id="field1" class="field" @click="addCodeEintrag('left')">
-          ←
-        </button>
-        <button id="field2" class="field" @click="addCodeEintrag('right')">
-          →
-        </button>
-        <button id="field3" class="field" @click="addCodeEintrag('up')">
-          ↑
-        </button>
-        <button id="field4" class="field" @click="addCodeEintrag('down')">
-          ↓
-        </button>
-        <button id="field5" class="field" @click="addCodeEintrag('A')">
-          A
-        </button>
-        <button id="field6" class="field" @click="addCodeEintrag('B')">
-          B
-        </button>
-        <button id="field7" class="field" @click="addCodeEintrag('Start')">
-          Start
-        </button>
-        <button id="field8" class="field" @click="addCodeEintrag('Select')">
-          Select
-        </button>
-      </div>
+  </div>
+  <div v-if="easterEggCounter >= 7">
+    <p>lol du hast mich gefunden</p>
+    <p>Gib nun den einzige wahren Cheat Code ein</p>
+    <!-- Div-Container für die Felder -->
+    {{ codeString }}
+    <div id="fieldsContainer" v-if="code.length < 10">
+      <!-- 8 Felder mit IDs field1 bis field8 -->
+      <button id="field1" class="field" @click="addCodeEintrag('left')">
+        ←
+      </button>
+      <button id="field2" class="field" @click="addCodeEintrag('right')">
+        →
+      </button>
+      <button id="field3" class="field" @click="addCodeEintrag('up')">↑</button>
+      <button id="field4" class="field" @click="addCodeEintrag('down')">
+        ↓
+      </button>
+      <button id="field5" class="field" @click="addCodeEintrag('A')">A</button>
+      <button id="field6" class="field" @click="addCodeEintrag('B')">B</button>
+      <button id="field7" class="field" @click="addCodeEintrag('Start')">
+        Start
+      </button>
+      <button id="field8" class="field" @click="addCodeEintrag('Select')">
+        Select
+      </button>
     </div>
   </div>
 </template>
@@ -93,7 +107,7 @@ function addCodeEintrag(wert) {
   code.value += wert;
   codeString.value += wert + ", ";
   if (code.value.length == 10) {
-    console.log("LOL")
+    console.log("LOL");
   }
 }
 </script>
